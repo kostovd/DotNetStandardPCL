6b65df92d0f3bd5b1056206bac497a29aca1a48d
dimitar-kostov <dimitar.i.kostov@gmail.com>
2017-04-04 14:12:16 +0300

    Add solution

---
+++ App/Program.cs
@@ -0,0 +1,33 @@
+﻿using Lib;
+using System;
+using System.Threading.Tasks;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunAsync().Wait();
+        }
+
+        static async Task RunAsync()
+        {
+            HttpClientWrapper clienttWrapper = new HttpClientWrapper();
+
+            Console.WriteLine("GetUserAsync");
+            string posts = await clienttWrapper.GetUserAsync();
+            Console.WriteLine("User string:");
+            Console.WriteLine(posts);
+
+            Console.WriteLine();
+
+            Console.WriteLine("GetUserAsyncJson");
+            dynamic postsJson = await clienttWrapper.GetUserAsyncJson();
+            Console.WriteLine("User json:");
+            Console.WriteLine(postsJson.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lib/HttpClientWrapper.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib
+{
+    public class HttpClientWrapper
+    {
+        private readonly HttpClient _httpClient;
+
+        public HttpClientWrapper()
+        {
+            _httpClient = new HttpClient();
+
+            _httpClient.BaseAddress = new Uri("https://jsonplaceholder.typicode.com");
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<string> GetUserAsync()
+        {
+            string result = string.Empty;
+
+            var response = await _httpClient.GetAsync("users/1");
+            if (response.IsSuccessStatusCode)
+            {
+                result = await response.Content.ReadAsStringAsync();
+            }
+
+            return result;
+        }
+
+        public async Task<dynamic> GetUserAsyncJson()
+        {
+            dynamic result = null;
+
+            var response = await _httpClient.GetAsync("users/1");
+            if (response.IsSuccessStatusCode)
+            {
+                using (var stream = await response.Content.ReadAsStreamAsync())
+                {
+                    result = ReadJson<dynamic>(stream);
+                }
+            }
+
+            return result;
+        }
+
+        private T ReadJson<T>(Stream stream)
+        {
+            using (var reader = new StreamReader(stream))
+            {
+                using (var jr = new JsonTextReader(reader))
+                {
+                    var jsonSerializer = new JsonSerializer();
+                    return jsonSerializer.Deserialize<T>(jr);
+                }
+            }
+        }
+    }
+}
