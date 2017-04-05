a7331575ac0be57258961a1ef11a907c95979975
dimitar-kostov <dimitar.i.kostov@gmail.com>
2017-04-05 18:25:13 +0300

    Added TestGetUserAsync

--- Tests/HttpClientWrapperTests.cs
+++ Tests/HttpClientWrapperTests.cs
@@ -43,5 +43,38 @@ namespace Tests
             var userJson = client.GetUserAsyncJson().Result;
             Assert.IsTrue(JToken.DeepEquals(jObject, userJson));
         }
+
+        [TestMethod]
+        public void TestGetUserAsync()
+        {
+            var jObject = new JObject()
+            {
+                { "Test1", 1 },
+                { "Test2", 2 },
+                { "Test3",
+                    new JArray
+                    {
+                        new JObject
+                        {
+                            { "Test4", "4" },
+                        },
+                    }
+                },
+            };
+
+            var mockHttp = new MockHttpMessageHandler();
+
+            string jsonResponse = jObject.ToString(Newtonsoft.Json.Formatting.None);
+
+            // Setup a respond for the user api (including a wildcard in the URL)
+            mockHttp.When("https://jsonplaceholder.typicode.com/users/*")
+                    .Respond(JsonMediaType, jsonResponse); // Respond with JSON
+
+            // Inject the handler into your application code
+            HttpClientWrapper client = new HttpClientWrapper(mockHttp);
+
+            var userJson = client.GetUserAsync().Result;
+            Assert.AreEqual(jObject.ToString(), userJson);
+        }
     }
 }
